769e1081feb6bef5a07c354b5e80e7028d0f75d7
Lawson <wlawsonkelly@gmail.com>
2021-12-03 13:46:41 -0800

    Initial Commit

---
+++ Shared/MessageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageAppApp.swift
+//  Shared
+//
+//  Created by Lawson Kelly  on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct MessageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
